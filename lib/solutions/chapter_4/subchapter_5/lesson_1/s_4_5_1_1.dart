0036b9faf7eb59d8c5f3f8636ea015ece5975c5a
Mert <cm.sapci@gmail.com>
2024-02-08 16:16:44 +0100

    erledigt

--- lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
@@ -4,6 +4,15 @@ class S4511 extends StatelessWidget {
   const S4511({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const HelloWorldTextWidget();
+  }
+}
+
+class HelloWorldTextWidget extends StatelessWidget {
+  const HelloWorldTextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello World');
   }
 }
