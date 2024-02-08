cadb18f40c5b07a7f02824296d4bee39f4b52dd3
Mert <cm.sapci@gmail.com>
2024-02-08 16:17:04 +0100

    erledigt

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,40 @@ class S4524 extends StatelessWidget {
   const S4524({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String nameText = '';
+  String buttonText = 'Name anzeigen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(nameText),
+        ElevatedButton(onPressed: (toggleName), child: Text(buttonText))
+      ],
+    );
+  }
+
+  void toggleName() {
+    setState(() {
+      if (nameText.isEmpty) {
+        nameText = 'Mert';
+        buttonText = 'Name verstecken';
+      } else {
+        nameText = '';
+        buttonText = 'Name anzeigen';
+      }
+    });
   }
 }
