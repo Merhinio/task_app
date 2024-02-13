a0b8720609177a75e743ae095c7ff4cc30115159
Mert <cm.sapci@gmail.com>
2024-02-13 16:31:54 +0100

    erledigt

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -4,6 +4,41 @@ class S4536 extends StatelessWidget {
   const S4536({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyProduct();
+  }
+}
+
+class Product {
+  String name;
+  int price;
+  Product(this.name, this.price);
+}
+
+class MyProduct extends StatelessWidget {
+  const MyProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Product> productList = [
+      Product('Product1', 10),
+      Product('Product2', 20),
+      Product('Product3', 30),
+      Product('Product4', 40),
+      Product('Product5', 50)
+    ];
+
+    return ListView.builder(
+      itemCount: productList.length,
+      shrinkWrap: true, //Apassung der größe liste
+      itemBuilder: (context, index) {
+        Product product = productList[index];
+        return ListTile(
+          //Struktur
+          leading: const Icon(Icons.shopping_cart), //Auf der Linkenseie des LEM
+          title: Text(product.name), //Titel
+          subtitle: Text('price: ${product.price}'), //Untertitel
+        );
+      }, // trailing (auf der rechten seite anzeigen )
+    );
   }
 }
