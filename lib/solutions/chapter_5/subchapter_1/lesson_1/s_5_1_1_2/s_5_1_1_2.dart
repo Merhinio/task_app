45ac98a73b8c54c2bf045a5f0b6a7a6489f10a10
Mert <cm.sapci@gmail.com>
2024-02-26 15:27:38 +0100

    bild hinzgefügt für die Aufgabe 5.1.1.2

--- lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
@@ -1,9 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/tasks/widget_extstyle.dart';
 
 class S5112 extends StatelessWidget {
   const S5112({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyWidget();
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Center(
+          child: Image(
+            image: AssetImage('assets/images/tasks/5.1.png'),
+          ),
+        ),
+        Text(
+          'Name',
+          style: titlestyle,
+        ),
+        Text(
+          'Moritz das Lahma',
+          style: contentstyle,
+        ),
+        Text(
+          'Gewicht',
+          style: titlestyle,
+        ),
+        Text(
+          '198 KG',
+          style: contentstyle,
+        ),
+        Text(
+          'Größe',
+          style: titlestyle,
+        ),
+        Text(
+          '1.80m',
+          style: contentstyle,
+        ),
+        Text(
+          'Lieblingsessen',
+          style: titlestyle,
+        ),
+        Text(
+          'Gras',
+          style: contentstyle,
+        )
+      ],
+    );
   }
 }
---
+++ lib/tasks/widget_extstyle.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const titlestyle = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    height: 2.5);
+const contentstyle =
+    TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.black);
